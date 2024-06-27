cab8fbd23f99db553976525d3830896f4f3fce6d
StevenCarrilloLoor <stevencarrilloloor@gmail.com>
2024-06-27 08:35:36 -0500

    Agregar archivos de proyecto.

---
+++ People/PersonRepository.cs
@@ -0,0 +1,102 @@
+﻿namespace People;
+using SQLite;
+using People.Models;
+
+public class PersonRepository
+{
+    string _dbPath;
+    private SQLiteConnection conn;
+
+    public string StatusMessage { get; set; }
+
+    // TODO: Add variable for the SQLite connection
+
+    private void Init()
+    {
+        // TODO: Add code to initialize the repository
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
+
+    }
+
+    public PersonRepository(string dbPath)
+    {
+        _dbPath = dbPath;                        
+    }
+
+    public void AddNewPerson(string name)
+    {            
+        int result = 0;
+        try
+        {
+            // enter this line
+            Init();
+
+            // basic validation to ensure a name was entered
+            if (string.IsNullOrEmpty(name))
+                throw new Exception("Valid name required");
+
+            // enter this line
+            result = conn.Insert(new Person { Name = name });
+
+            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+        }
+
+    }
+
+    public List<Person> GetAllPeople()
+    {
+        // TODO: Init then retrieve a list of Person objects from the database into a list
+        try
+        {
+            Init();
+            return conn.Table<Person>().ToList();
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+
+        }
+
+        return new List<Person>();
+    }
+    //codigo implementado por el profesor
+
+    public Person GetPerson(int id)
+    {
+        try
+        {
+            Init();
+            var people =  conn.Table<Person>().ToList();
+            Person person = people.Find(x => x.Id == id);
+            return person;
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+
+        }
+        return new Person();
+    }
+    public  void DetachPerson(Person person)
+    {
+        try
+        {
+            Init();
+            conn.Delete(person);
+            
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to delete data. {0}", ex.Message);
+
+        }
+    }
+}
